805d0db7692c88b96ebaa62f6aca453c1e3eb57c
novaeco <novatribolnovea@gmail.com>
2025-06-13 17:04:36 +0200

    Delete components directory

--- components/LovyanGFX/LovyanGFX.cpp
+++
@@ -1,6 +0,0 @@
-#include "LovyanGFX.hpp"
-
-// LovyanGFX.cpp - This file can be mostly empty as the configuration is in LGFX_Config.hpp
-// If you need to add custom drawing functions or extensions, they would go here.
-
-
